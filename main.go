d5d07a4f2d52570fc8778ac1f31ad30a65704509
Kcrong <iam@devkcr.org>
2024-03-31 22:47:42 +0900

    Update

--- main.go
+++ main.go
@@ -45,6 +45,13 @@ func main() {
 	r := gin.Default()
 	r.Use(sentrygin.New(sentrygin.Options{}))
 
+	r.GET("/", func(c *gin.Context) {
+		c.Data(http.StatusOK, "text/html; charset=utf-8", htmlIndex)
+	})
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "https://raw.githubusercontent.com/gin-gonic/examples/master/favicon/favicon.ico")
+	})
+
 	r.GET("/press", func(c *gin.Context) {
 		res, err := device.PostDeviceCommand(deviceID, []device.Command{
 			{
@@ -77,3 +84,44 @@ func main() {
 		logrus.Fatalf("%+v", err)
 	}
 }
+
+var (
+	htmlIndex = []byte(`<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>배송 안내</title>
+</head>
+
+<body>
+
+<h1>배송 안내</h1>
+<p>인터폰을 통해 호수의 호출을 눌러주세요.</p>
+<p>예) 101호의 경우, "101" 입력 후 "호출" 클릭</p>
+<p>호출 대기음이 들리면, 아래 버튼을 눌러주세요.</p>
+
+<input id="pressing_button" style="height:200px;width:200px" type="submit" value="호출 수락하기" onclick="submit()">
+
+<script type="text/javascript">
+    function submit() {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+				document.getElementById("pressing_button").disabled = false;
+				document.getElementById("pressing_button").value = "호출 수락하기";
+                alert('완료되었습니다.');
+            } else {
+				document.getElementById("pressing_button").disabled = true;
+				document.getElementById("pressing_button").value = "호출 수락 중...";
+			}
+        }
+        xhr.open('GET', 'press', true);
+        xhr.send();
+    }
+
+</script>
+</body>
+</html>
+`)
+)
