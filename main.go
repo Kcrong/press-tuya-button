bd195eeaa25588412571fe035d12f83fdf7ddd72
Henry Kim <Kcrong@users.noreply.github.com>
2024-03-31 21:16:02 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/tuya/tuya-cloud-sdk-go/api/common"
+	"github.com/tuya/tuya-cloud-sdk-go/api/device"
+	"github.com/tuya/tuya-cloud-sdk-go/config"
+	"net/http"
+	"os"
+)
+
+const (
+	deviceID  = "removed"
+	accessID  = "removed"
+	accessKey = "removed"
+
+	defaultPort = "8012"
+)
+
+func main() {
+	config.SetEnv(
+		common.URLUS,
+		accessID,
+		accessKey,
+	)
+
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn:           "removed",
+		EnableTracing: true,
+		// Set TracesSampleRate to 1.0 to capture 100%
+		// of transactions for performance monitoring.
+		// We recommend adjusting this value in production,
+		TracesSampleRate: 1.0,
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %+v", errors.WithStack(err))
+	}
+
+	r := gin.Default()
+	r.Use(sentrygin.New(sentrygin.Options{}))
+
+	r.GET("/press", func(c *gin.Context) {
+		res, err := device.PostDeviceCommand(deviceID, []device.Command{
+			{
+				Code:  "switch",
+				Value: true,
+			},
+		})
+
+		if err == nil && res.Success {
+			c.Status(http.StatusNoContent)
+			return
+		}
+
+		if err != nil {
+			sentry.CaptureException(errors.WithStack(err))
+		}
+		if !res.Success {
+			sentry.CaptureMessage("failed to press: " + fmt.Sprintf("%+v", res))
+		}
+
+		c.Status(http.StatusInternalServerError)
+	})
+
+	port := os.Getenv("GIN_PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	if err := r.Run(":" + port); err != nil {
+		logrus.Fatalf("%+v", err)
+	}
+}
